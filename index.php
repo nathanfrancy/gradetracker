43a1b770bd6ed62c9acf9340273d92a5766f8503
Nathan Francy <nathanfrancy@gmail.com>
2014-12-19 21:49:20 -0600

    initial import

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="assets/vendor/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="assets/vendor/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/vendor/js/bootstrap.min.js"></script>
+</body>
+
+</html>
