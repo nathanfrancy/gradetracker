f13746b8b5e4427e4a108d1c916b1fa1d9ebd599
Nathan Francy <nathanfrancy@gmail.com>
2015-01-18 15:49:56 -0600

    Checking session for logged in in get.php in api

--- api/get.php
+++ api/get.php
@@ -7,58 +7,65 @@ require("../data/master.php");
 $response = null;
 
 $id = null;
-$auth_id = $_SESSION['auth_id'];
-
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 }
 
-// Check if there is a request type defined in the request
-if (isset($_GET['rq'])) {
-    
-    $rq = $_GET['rq'];
-    
-    /* Start of long if-statements to determine which request type 
-        the user is wanting. If the request type is not a valid one,
-        is puts a warning message in the response.
-    */
-    if ($rq === "getAllSchoolYears") {
-        $schoolYears = getAllSchoolYears($auth_id);
-        $response = $schoolYears;
+// Check if a session auth_id is present. Must be logged in at the very least
+// to view any type of data in the system. Otherwise get an error.
+if (isset($_SESSION['auth_id'])) {
+    $auth_id = $_SESSION['auth_id'];
+
+    // Check if there is a request type defined in the request
+    if (isset($_GET['rq'])) {
+        
+        $rq = $_GET['rq'];
+        
+        /* Start of long if-statements to determine which request type 
+            the user is wanting. If the request type is not a valid one,
+            is puts a warning message in the response.
+        */
+        if ($rq === "getAllSchoolYears") {
+            $schoolYears = getAllSchoolYears($auth_id);
+            $response = $schoolYears;
+        }
+        else if ($rq === "getSchoolYear") {
+            $schoolyear = getSchoolYear($id);
+            $response = $schoolyear;
+        }
+        else if ($rq === "getStudentsFromSchoolYear") {
+            $students = getStudentsFromSchoolYear($id);
+            $response = $students;
+        }
+        else if ($rq === "getStudent") {
+            $student = getStudent($id);
+            $response = $student;
+        }
+        else if ($rq === "getStandardsFromSchoolYear") {
+            $standards = getStandardsFromSchoolYear($id);
+            $response = $standards;
+        }
+        else if ($rq === "getStandard") {
+            $standard = getStandard($id);
+            $response = $standard;
+        }
+        else if ($rq === "getStudentsWithStandardGrades") {
+            $students = getStudentsWithStandardGrades($_GET['schoolyear_id'], $_GET['standard_id']);
+            $response = $students;
+        }
+        else {
+            $response['response'] = "fail";
+            $response['message'] = "Request type not valid.";
+        }
     }
-    else if ($rq === "getSchoolYear") {
-        $schoolyear = getSchoolYear($id);
-        $response = $schoolyear;
-    }
-    else if ($rq === "getStudentsFromSchoolYear") {
-        $students = getStudentsFromSchoolYear($id);
-        $response = $students;
-    }
-    else if ($rq === "getStudent") {
-        $student = getStudent($id);
-        $response = $student;
-    }
-    else if ($rq === "getStandardsFromSchoolYear") {
-        $standards = getStandardsFromSchoolYear($id);
-        $response = $standards;
-    }
-    else if ($rq === "getStandard") {
-        $standard = getStandard($id);
-        $response = $standard;
-    }
-    else if ($rq === "getStudentsWithStandardGrades") {
-        $students = getStudentsWithStandardGrades($_GET['schoolyear_id'], $_GET['standard_id']);
-        $response = $students;
-    }
-    
     else {
         $response['response'] = "fail";
-        $response['message'] = "Request type not valid.";
-    }
+        $response['message'] = "No request type (rq) defined in request.";
+    } 
 }
 else {
     $response['response'] = "fail";
-    $response['message'] = "No request type (rq) defined in request.";
+    $response['message'] = "You must be logged in to view this data.";
 }
 
 
